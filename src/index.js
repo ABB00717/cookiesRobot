df6fc3384f07bb1c3e4860b4baf6aaaf6d0232ef
ABB00717 <eric94528@gmail.com>
2023-03-05 21:48:28 +0800

    ver.1.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+import vueInit from './core/vue.js'
 import dotenv from 'dotenv';
+
 dotenv.config({path: '../.env'});
+vueInit();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
